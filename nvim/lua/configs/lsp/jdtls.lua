acbd54afff29803f0ada8e6502ce0f7fae6adb08
MrEllen_ <56396869+MrEll3n@users.noreply.github.com>
2025-11-20 16:22:09 +0100

    Delete nvim directory

--- nvim/lua/configs/lsp/jdtls.lua
+++
@@ -1,8 +0,0 @@
--- Minimální JDT. Pro pokročilé features (code actions, testy, debug) doporučuji plugin "nvim-jdtls".
-return {
-  -- mason-lspconfig nastaví cmd/root_dir automaticky
-  on_attach = function(client, bufnr)
-    -- sem si můžeš dát Java-specifické mapy; NvChad on_attach se už přiklapí v loaderu
-  end,
-}
-
--- nvim/lua/configs/lsp/rust_analyzer.lua
+++
@@ -1,9 +0,0 @@
-return {
-  settings = {
-    ["rust-analyzer"] = {
-      cargo = { allFeatures = true },
-      procMacro = { enable = true },
-      checkOnSave = { command = "clippy" },
-    },
-  },
-}
